203c50cf58aab35ac35ec4663c17cf2e1bd79304
Kaito <kaito.kawasaki09@gmail.com>
2024-11-23 17:26:58 +0900

    アイコン調整

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  [
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50",
+    "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black", // outline-2に変更
+    "ring-offset-background",
+  ].join(" "),
   {
     variants: {
       variant: {
@@ -22,7 +25,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-9 w-9 p-2",
       },
     },
     defaultVariants: {
