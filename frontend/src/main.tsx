a189f03bbeaec283fdf8a1f0a4abde3c830b9ff1
Kaito <kaito.kawasaki09@gmail.com>
2024-11-23 17:51:44 +0900

    ルーティング変更

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App_temp";
+import App_temp from "./App_temp";
 import "./output.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App_temp />
   </StrictMode>
 );
