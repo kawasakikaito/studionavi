97d5b4ce52fa0efe5780540bd95922aa8cbf3bc2
Kaito <kaito.kawasaki09@gmail.com>
2024-12-21 12:26:31 +0900

    テストデータ作成のfactoy追加

---
+++ backend/api/factories.py
@@ -0,0 +1,26 @@
+import factory
+from datetime import time, timedelta
+from django.utils import timezone
+from api.models import Studio
+
+class StudioFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Studio
+
+    name = factory.Sequence(lambda n: f'スタジオ{n+1}')
+    address = factory.Sequence(lambda n: f'東京都渋谷区代々木{n+1}-{n+1}-{n+1}')
+    
+    # 営業時間は10:00-22:00をデフォルトとする
+    opening_time = factory.LazyFunction(lambda: time(10, 0))
+    closing_time = factory.LazyFunction(lambda: time(22, 0))
+    closes_next_day = False
+    
+    reservation_url = factory.Sequence(lambda n: f'https://example.com/studio/{n+1}')
+    
+    # 予約開始タイミング: デフォルトで7日前から予約可能
+    self_practice_reservation_start_date = factory.LazyFunction(lambda: timedelta(days=7))
+    # 予約開始時間: デフォルトで24時間前から予約可能
+    self_practice_reservation_start_time = factory.LazyFunction(lambda: timedelta(hours=24))
+    
+    created_at = factory.LazyFunction(timezone.now)
+    updated_at = factory.LazyFunction(timezone.now)
