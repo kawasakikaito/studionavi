aeda2531a39c82708a9699f3f6dffafcc6fba2a0
Kaito <kaito.kawasaki09@gmail.com>
2024-12-22 09:50:15 +0900

    検索結果を関連度順でソート（完全一致 > 前方一致 > 部分一致） 検索結果の最大返却件数を10件に制限

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from django.db.models import Q
+from django.db.models import Q, Case, When
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import *
@@ -9,9 +9,14 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+from django.db.models import Q, Value, FloatField
+from django.db.models.functions import Greatest, Cast
+from django.db.models.functions import Length
+
 class StudioViewSet(viewsets.ModelViewSet):
     queryset = Studio.objects.all()
     serializer_class = StudioSerializer
+    result_limit = 10
 
     @action(detail=False, methods=['get'])
     def search(self, request):
@@ -19,9 +24,35 @@ class StudioViewSet(viewsets.ModelViewSet):
         if not query:
             return Response([])
             
-        queryset = self.get_queryset().filter(
+        # 検索クエリとの関連度をスコア化
+        queryset = self.get_queryset().annotate(
+            relevance=Greatest(
+                # 完全一致は最高スコア
+                Case(
+                    When(name__iexact=query, then=Value(100)),
+                    When(address__iexact=query, then=Value(100)),
+                    default=Value(0),
+                    output_field=FloatField(),
+                ),
+                # 前方一致は次に高いスコア
+                Case(
+                    When(name__istartswith=query, then=Value(80)),
+                    When(address__istartswith=query, then=Value(80)),
+                    default=Value(0),
+                    output_field=FloatField(),
+                ),
+                # 部分一致は最低スコア
+                Case(
+                    When(name__icontains=query, then=Value(60)),
+                    When(address__icontains=query, then=Value(60)),
+                    default=Value(0),
+                    output_field=FloatField(),
+                ),
+            )
+        ).filter(
             Q(name__icontains=query) |
             Q(address__icontains=query)
-        )
+        ).order_by('-relevance')[:self.result_limit]
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
