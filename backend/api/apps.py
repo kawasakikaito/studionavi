753ce5c0c8db708326b34d510b79d0fb7dd3578c
Kaito <kaito.kawasaki09@gmail.com>
2025-01-03 19:18:42 +0900

    レジストリの初期化処理をapps.pyに追加

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
+
+    def ready(self):
+        """
+        Djangoアプリケーション起動時にスクレイパーを初期化
+        """
+        from .scrapers.scraper_register import AvailabilityService
+        
+        service = AvailabilityService()
+        service.initialize_scrapers()
