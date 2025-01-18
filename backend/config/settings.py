3447877dad0fc273b929ac7abeb550b5a18ca3d7
Kaito <kaito.kawasaki09@gmail.com>
2025-01-18 12:56:29 +0900

    djangorestframework_camel_caseの導入

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,24 @@ INSTALLED_APPS = [
     "rest_framework",
     "api",
     'corsheaders',
+    'djangorestframework_camel_case', 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+        # Any other renders
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        # If you use MultiPartFormParser or FormParser, we also have a camel case version
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+        # Any other parsers
+    ),
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
